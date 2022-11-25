3303a6d1f4b74cfdf73b6619af9b5e68bce05a76
johnwilly990 <j.kim1940@gmail.com>
2022-11-24 22:19:21 -0500

    Add controller for getting single warehouse

--- routes/warehouse.js
+++ routes/warehouse.js
@@ -1,29 +1,21 @@
 // require variables
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
 
 // Controllers
-const warehouseController = require('../controllers/warehouseController')
+const warehouseController = require("../controllers/warehouseController");
 
 router
-    .route('/')
-        .get(warehouseController.getAll)
-        .post(warehouseController.addNew); // CREATE new warehouse
-        
-router
-    .route('/:id')
-    .get((req, res) => {
-
-    })
-    .put((req, res) => {
-
-    })
-    .delete((req, res) => {
-
-    })
+  .route("/")
+  .get(warehouseController.getAll)
+  .post(warehouseController.addNew); // CREATE new warehouse
 
 router
-    .route('/:id/inventories')
-    .get(warehouseController.warehouseInventories); // GET list of inventories for a specific warehouse
+  .route("/:id")
+  .get(warehouseController.getSingleWarehouse)
+  .put((req, res) => {})
+  .delete((req, res) => {});
+
+router.route("/:id/inventories").get(warehouseController.warehouseInventories); // GET list of inventories for a specific warehouse
 
 module.exports = router;
